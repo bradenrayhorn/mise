d22fb0add96ef4f8ad5382bf2f4f687110521ead
Braden Rayhorn <25675893+bradenrayhorn@users.noreply.github.com>
2024-05-10 21:07:56 -0500

    Add basic datastore and sqlite backend

--- server/src/bin/server.rs
+++ server/src/bin/server.rs
@@ -1,11 +1,23 @@
-use mise::{config::Config, http::Server};
+use mise::{config::Config, datastore, http::Server, sqlite};
 
 #[tokio::main]
 async fn main() {
     let config = Config { port: 3000 };
-    let s = Server::new(config);
+    let (_worker_pool, senders) = match sqlite::worker_pool() {
+        Ok(pool) => pool,
+        Err(err) => {
+            println!("error with pool: {:?}", err);
+            return;
+        }
+    };
+    let pool = datastore::Pool::new(senders);
+
+    let s = Server::new(config, pool);
 
     if let Err(err) = s.start().await {
         println!("Failed to start http server: {:?}", err)
     }
+
+    // TODO - graceful shutdown of pool and http server.
+    // can sort of test the sqlite graceful shutdown by seeing if the wal and shm files are deleted
 }
---
+++ server/src/datastore.rs
@@ -0,0 +1,110 @@
+use std::sync::{mpsc, Arc, Mutex};
+
+use anyhow::anyhow;
+use thiserror::Error;
+use tokio::sync::oneshot;
+
+use crate::domain::User;
+
+// Error
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("record not found")]
+    NotFound,
+
+    #[error("not in a transaction")]
+    NotTransaction,
+
+    #[error("no available connections")]
+    NoConnections,
+
+    #[error(transparent)]
+    Unknown(#[from] anyhow::Error),
+}
+
+impl From<tokio::sync::oneshot::error::RecvError> for Error {
+    fn from(value: tokio::sync::oneshot::error::RecvError) -> Self {
+        Error::Unknown(anyhow!(value))
+    }
+}
+
+// Pool
+
+#[derive(Clone)]
+pub struct Pool {
+    // TODO - More efficient locking and data structure could be used, but this is fine for now.
+    connections: Arc<Mutex<Vec<mpsc::Sender<DatastoreMessage>>>>,
+}
+
+#[derive(Clone)]
+struct PoolConnection {
+    sender: mpsc::Sender<DatastoreMessage>,
+    connections: Arc<Mutex<Vec<mpsc::Sender<DatastoreMessage>>>>,
+}
+
+impl Drop for PoolConnection {
+    fn drop(&mut self) {
+        // release thread back to pool when connection is done
+        let mut connections = self.connections.lock().unwrap();
+        connections.push(self.sender.clone())
+    }
+}
+
+impl Pool {
+    pub fn new(connections: Vec<mpsc::Sender<DatastoreMessage>>) -> Self {
+        Pool {
+            connections: Arc::new(Mutex::new(connections)),
+        }
+    }
+
+    fn conn(&self) -> Result<PoolConnection, Error> {
+        // TODO - should check if the connection is healthy before use.
+        // - benchmark the change before and after to make sure performance is still okay.
+        let mut connections = self.connections.lock().unwrap();
+        match connections.pop() {
+            Some(conn) => Ok(PoolConnection {
+                sender: conn,
+                connections: self.connections.clone(),
+            }),
+            None => Err(Error::NoConnections),
+        }
+    }
+
+    pub async fn get_user(&self, id: String) -> Result<User, Error> {
+        let conn = self.conn()?;
+        let (tx, rx) = oneshot::channel();
+        let msg = DatastoreMessage::GetUser { id, respond_to: tx };
+
+        let _ = conn.sender.send(msg);
+        rx.await?
+    }
+
+    pub async fn insert_user(&self, id: String, username: String) -> Result<(), Error> {
+        let conn = self.conn()?;
+        let (tx, rx) = oneshot::channel();
+        let msg = DatastoreMessage::InsertUser {
+            id,
+            username,
+            respond_to: tx,
+        };
+
+        let _ = conn.sender.send(msg);
+        rx.await?
+    }
+}
+
+pub enum DatastoreMessage {
+    Health {
+        respond_to: oneshot::Sender<Result<(), Error>>,
+    },
+    GetUser {
+        id: String,
+        respond_to: oneshot::Sender<Result<User, Error>>,
+    },
+    InsertUser {
+        id: String,
+        username: String,
+        respond_to: oneshot::Sender<Result<(), Error>>,
+    },
+}
