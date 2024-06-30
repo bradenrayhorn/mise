4361a02e10c7e1cd34dff86a652f8a77e2bc2550
Braden Rayhorn <25675893+bradenrayhorn@users.noreply.github.com>
2024-06-30 11:41:27 -0500

    Add favicon and Apple meta

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -9,9 +9,9 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/api': 'http://localhost:3000',
-      '/auth': 'http://localhost:3000',
-      '^/$': 'http://localhost:3000',
+      '/api': 'http://127.0.0.1:3000',
+      '/auth': 'http://127.0.0.1:3000',
+      '^/$': 'http://127.0.0.1:3000',
     },
   },
 });
