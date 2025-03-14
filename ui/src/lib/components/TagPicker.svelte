4378f644972896e97aec1406180fb2dbac5765f3
Braden Rayhorn <25675893+bradenrayhorn@users.noreply.github.com>
2025-03-13 20:38:19 -0500

    Use zagjs for tag picker

--- ui/src/lib/components/TagPicker.svelte
+++ ui/src/lib/components/TagPicker.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   import type { Tag } from '$lib/types/tag';
-  import { createDropdownMenu, melt } from '@melt-ui/svelte';
+  import * as menu from '@zag-js/menu';
   import TagModal from './recipes/form/TagModal.svelte';
+  import { useMachine, normalizeProps, portal } from '@zag-js/svelte';
 
   type Props = {
     tags: Array<Tag>;
@@ -11,49 +12,51 @@
 
   let { tags, onselect, canCreate = false }: Props = $props();
 
-  const {
-    elements: { trigger, menu, item, separator },
-    states: { open },
-  } = createDropdownMenu({
-    loop: true,
+  const id = $props.id();
+  const service = useMachine(menu.machine, {
+    id,
+    typeahead: true,
+    positioning: { sameWidth: true },
+    onSelect: (details) => {
+      if (!details.value.startsWith('*')) {
+        onselect({ tagID: details.value });
+      }
+    },
   });
-
-  function onSelect(id: string) {
-    onselect({ tagID: id });
-  }
+  const api = $derived(menu.connect(service, normalizeProps));
 </script>
 
 <button
-  use:melt={$trigger}
+  {...api.getTriggerProps()}
+  data-active={api.open}
   class="btn-solid btn-gray w-full text-sm text-center py-1"
-  data-active={$open}
 >
   Add Tag
 </button>
 
-{#if $open}
+{#if api.open}
   <div
-    class="z-10 max-h-60 overflow-y-auto flex flex-col shadow rounded bg-base-600 p-1 min-w-40"
-    use:melt={$menu}
+    class="z-10 shadow rounded bg-base-600 p-1 min-w-40"
+    use:portal
+    {...api.getPositionerProps()}
   >
-    {#each tags as tag (tag.id)}
-      <button
-        class="pl-3 data-[highlighted]:bg-base-primaryHover py-1 text-left"
-        use:melt={$item}
-        onclick={() => {
-          onSelect(tag.id);
-        }}
-      >
-        {tag.name}
-      </button>
-    {:else}
-      No options
-    {/each}
+    <div class="overflow-y-auto flex flex-col max-h-60" {...api.getContentProps()}>
+      {#each tags as tag (tag.id)}
+        <button
+          class="pl-3 data-[highlighted]:bg-base-primaryHover py-1 text-left"
+          {...api.getItemProps({ value: tag.id })}
+        >
+          {tag.name}
+        </button>
+      {:else}
+        No options
+      {/each}
 
-    {#if canCreate}
-      <div class="h-px shrink-0 bg-divider-default my-2" use:melt={$separator}></div>
+      {#if canCreate}
+        <div class="h-px shrink-0 bg-divider-default my-2" {...api.getSeparatorProps()}></div>
 
-      <TagModal element={item} />
-    {/if}
+        <TagModal {...api.getItemProps({ value: '*open-tag-modal' })} />
+      {/if}
+    </div>
   </div>
 {/if}
--- ui/src/lib/components/recipes/form/TagModal.svelte
+++ ui/src/lib/components/recipes/form/TagModal.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import IconClose from '~icons/mdi/close';
-  import { createDialog, melt, type AnyMeltElement } from '@melt-ui/svelte';
   import { defaults, superForm } from 'sveltekit-superforms';
   import { zod } from 'sveltekit-superforms/adapters';
   import { schema } from './tag-schema';
@@ -8,19 +7,19 @@
   import Button from '$lib/components/Button.svelte';
   import { useQueryClient } from '@tanstack/svelte-query';
   import { queryKeys } from '$lib/api/query-keys';
+  import * as dialog from '@zag-js/dialog';
+  import { portal, normalizeProps, useMachine } from '@zag-js/svelte';
+  import type { HTMLAttributes } from 'svelte/elements';
 
-  interface Props {
-    element: AnyMeltElement;
-  }
+  type Props = HTMLAttributes<HTMLElement>;
 
-  const { element }: Props = $props();
+  const rest: Props = $props();
 
   const client = useQueryClient();
 
-  const {
-    elements: { trigger, portalled, overlay, content, title, close },
-    states: { open },
-  } = createDialog({ closeOnOutsideClick: false });
+  const id = $props.id();
+  const service = useMachine(dialog.machine, { id });
+  const api = $derived(dialog.connect(service, normalizeProps));
 
   const { form, errors, enhance, submitting } = superForm(defaults(zod(schema)), {
     SPA: true,
@@ -39,67 +38,65 @@
 
         await client.invalidateQueries({ queryKey: [queryKeys.tag.list] });
 
-        $open = false;
+        api.setOpen(false);
       }
     },
   });
 </script>
 
 <button
-  use:melt={$element}
-  use:melt={$trigger}
+  {...rest}
+  {...api.getTriggerProps()}
   class="pl-3 w-full text-left data-[highlighted]:bg-base-primaryHover">Create Tag</button
 >
 
-{#if $open}
-  <div use:melt={$portalled}>
-    <div
-      use:melt={$overlay}
-      class="fixed z-40 bg-base-backdrop top-0 bottom-0 right-0 left-0"
-      aria-hidden="true"
-      onclick={(e) => {
-        e.stopPropagation();
-        $open = false;
-      }}
-      transition:fade={{ duration: 100 }}
-    ></div>
-    <div
-      use:melt={$content}
-      class="fixed z-50 bottom-0 left-0 right-0 md:bottom-1/2 md:left-1/2 md:-translate-x-1/2 bg-base-500 rounded-t-xl md:rounded-lg md:max-w-96 flex flex-col"
-      transition:fade={{ duration: 100 }}
-    >
-      <form method="POST" use:enhance>
-        <div class="flex items-center p-4 border-b-divider-default border-b mb-4 shrink-0">
-          <div class="flex-1 flex items-center">
-            <button use:melt={$close} class="rounded-full text-fg-muted p-1 text-lg"
-              ><IconClose /></button
-            >
-          </div>
-          <h2 use:melt={$title} class="font-semibold">Create Tag</h2>
-          <div class="flex-1"></div>
-        </div>
-
-        <div class="px-4 mb-6">
-          <label>
-            Name
-            <input
-              class="input"
-              aria-invalid={$errors.name ? 'true' : undefined}
-              bind:value={$form.name}
-            />
-          </label>
-          {#if $errors.name}<span class="invalid">{$errors.name}</span>{/if}
-        </div>
-
-        <div class="shrink-0 p-4 flex gap-2">
-          <button use:melt={$close} class="btn-solid btn-gray btn-sm grow" disabled={$submitting}
-            >Cancel</button
-          >
-          <Button type="submit" class="btn-solid btn-primary btn-sm grow" isLoading={$submitting}
-            >Save</Button
+{#if api.open}
+  <div
+    use:portal
+    {...api.getBackdropProps()}
+    class="fixed z-40 bg-base-backdrop top-0 bottom-0 right-0 left-0"
+    aria-hidden="true"
+    transition:fade={{ duration: 100 }}
+  ></div>
+  <div
+    use:portal
+    {...api.getPositionerProps()}
+    class="fixed z-50 bottom-0 left-0 right-0 md:bottom-1/2 md:left-1/2 md:-translate-x-1/2 bg-base-500 rounded-t-xl md:rounded-lg md:max-w-96 flex flex-col"
+    transition:fade={{ duration: 100 }}
+  >
+    <form method="POST" use:enhance {...api.getContentProps()}>
+      <div class="flex items-center p-4 border-b-divider-default border-b mb-4 shrink-0">
+        <div class="flex-1 flex items-center">
+          <button {...api.getCloseTriggerProps()} class="rounded-full text-fg-muted p-1 text-lg"
+            ><IconClose /></button
           >
         </div>
-      </form>
-    </div>
+        <h2 {...api.getTitleProps()} class="font-semibold">Create Tag</h2>
+        <div class="flex-1"></div>
+      </div>
+
+      <div class="px-4 mb-6">
+        <label>
+          Name
+          <input
+            class="input"
+            aria-invalid={$errors.name ? 'true' : undefined}
+            bind:value={$form.name}
+          />
+        </label>
+        {#if $errors.name}<span class="invalid">{$errors.name}</span>{/if}
+      </div>
+
+      <div class="shrink-0 p-4 flex gap-2">
+        <button
+          {...api.getCloseTriggerProps()}
+          class="btn-solid btn-gray btn-sm grow"
+          disabled={$submitting}>Cancel</button
+        >
+        <Button type="submit" class="btn-solid btn-primary btn-sm grow" isLoading={$submitting}
+          >Save</Button
+        >
+      </div>
+    </form>
   </div>
 {/if}
